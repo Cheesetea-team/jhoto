f814678db8365649a54d789653974dea2ee0a46a
ronaldo <ronaldo@cheesetea.com>
2021-06-12 12:00:06 +0200

    Crates pueden tener siblings y se mueven juntos

--- src/actors/Crate.gd
+++ src/actors/Crate.gd
@@ -1,10 +1,18 @@
 extends Actor
 
-var movement = Vector2()
+export (NodePath) var sibling_path = null
+var sibling_node = null
+var movement: = Vector2()
+
+func _ready() -> void:
+	if sibling_path:
+		sibling_node = get_node(sibling_path)
 
 func _physics_process(_delta: float) -> void:
 	if movement:
 		move_and_slide(movement)
+		if sibling_node:
+			sibling_node.move_and_slide(movement)
 		movement = Vector2.ZERO
 	
 func _on_Player_pushbody(colid, linear_vel) -> void:
