36cb5984ceade046be8ce23ddea7f8e7e60c59e1
hector <hlinares@ecomotriz.com>
2021-06-12 22:17:40 +0200

    Added marker for the link Some modifications to levels (visuals)

--- src/actors/Crate.gd
+++ src/actors/Crate.gd
@@ -2,11 +2,24 @@ extends Actor
 
 export (NodePath) var sibling_path = null
 var sibling_node = null
+var marker = null
 var movement: = Vector2()
 
 func _ready() -> void:
 	if sibling_path:
 		sibling_node = get_node(sibling_path)
+		marker = get_node("Marker")
+		var target = sibling_node.global_position - marker.global_position
+		var angle = (atan2(target.y, target.x))
+		marker.rotation = angle
+
+func _process(delta):
+	if sibling_path:
+		sibling_node = get_node(sibling_path)
+		marker = get_node("Marker")
+		var target = sibling_node.global_position - marker.global_position
+		var angle = (atan2(target.y, target.x))
+		marker.rotation = angle + PI/2
 
 func _physics_process(_delta: float) -> void:
 	var _err
