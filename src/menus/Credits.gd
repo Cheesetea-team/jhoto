ae77c849fb098fad3b7eb1e0dbaaa6b9097f29b5
ronaldo <ronaldo@cheesetea.com>
2021-06-13 00:40:32 +0200

    Teclas actualizadas en créditos

--- src/menus/Credits.gd
+++ src/menus/Credits.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 export var main_menu_scene = "res://src/menus/menu_principal.tscn"
 
 func _process(_delta: float) -> void:
-	var cancel = Input.is_action_pressed("ui_cancel")
-	var accept = Input.is_action_pressed("ui_accept")
+	var cancel = Input.is_action_pressed("exit")
+	var accept = Input.is_action_pressed("continue")
 	if cancel or accept:
 		if !get_tree().change_scene(main_menu_scene):
 			print("Can't change scene!")
