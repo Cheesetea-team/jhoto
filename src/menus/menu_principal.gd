7d6c095560a99a5c92683b191fb646a9d2612675
ronaldo <ronaldo@cheesetea.com>
2021-06-13 10:40:14 +0200

    Intentos de arreglar el menú y carpetas de export

--- src/menus/menu_principal.gd
+++ src/menus/menu_principal.gd
@@ -1,17 +1,26 @@
-extends TextureRect
+extends Node2D
 
-export var play_scene: 		= "res://src/levels/level_1.tscn"
+export var play_scene: 		= "res://src/levels/level_0.tscn"
 export var credits_scene: 	= "res://src/menus/Credits.tscn"
 
 func _on_Boton_jugar_pressed() -> void:
-	if !get_tree().change_scene(play_scene):
+	if get_tree().change_scene(play_scene) != OK:
 		print("Can't change scene!")
 
 
 func _on_Boton_Creditos_pressed() -> void:
-	if !get_tree().change_scene(credits_scene):
+	if get_tree().change_scene(credits_scene) != OK:
 		print("Can't change scene!")
 
 
 func _on_Boton_salir_pressed() -> void:
 	get_tree().quit(0)
+
+
+func _on_TextureButton_pressed() -> void:
+	get_tree().change_scene(play_scene)
+
+
+func _on_Boton_jugar_button_down() -> void:
+	if get_tree().change_scene(play_scene) != OK:
+		print("Can't change scene!")
