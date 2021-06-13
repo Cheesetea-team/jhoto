289a3481ffdca487bdac3131cabeecb689771b4d
Joaquin <raymost@cheesetea.com>
2021-06-13 02:16:49 +0200

    BAD Music added!!

--- src/menus/menu_principal.gd
+++ src/menus/menu_principal.gd
@@ -4,14 +4,17 @@ export var play_scene: 		= "res://src/levels/level_1.tscn"
 export var credits_scene: 	= "res://src/menus/Credits.tscn"
 
 func _on_Boton_jugar_pressed() -> void:
+	$MenuMusic.stop()
 	if !get_tree().change_scene(play_scene):
 		print("Can't change scene!")
 
 
 func _on_Boton_Creditos_pressed() -> void:
+	$MenuMusic.stop()
 	if !get_tree().change_scene(credits_scene):
 		print("Can't change scene!")
 
 
 func _on_Boton_salir_pressed() -> void:
+	$MenuMusic.stop()
 	get_tree().quit(0)
