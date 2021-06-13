8b605bbb2134ecb2742afbf0504c77cca1814744
ronaldo <ronaldo@cheesetea.com>
2021-06-13 13:37:48 +0200

    Fixed nivel donde no iba animación de entrada

--- src/menus/menu_principal.gd
+++ src/menus/menu_principal.gd
@@ -3,6 +3,9 @@ extends TextureRect
 export var play_scene: 		= "res://src/menus/controls.tscn"
 export var credits_scene: 	= "res://src/menus/Credits.tscn"
 
+func _ready() -> void:
+	$Boton_jugar.grab_focus()
+
 func _on_Boton_jugar_pressed() -> void:
 	$MenuMusic.stop()
 	if get_tree().change_scene(play_scene) != OK:
@@ -16,3 +19,15 @@ func _on_Boton_Creditos_pressed() -> void:
 func _on_Boton_salir_pressed() -> void:
 	$MenuMusic.stop()
 	get_tree().quit(0)
+
+
+func _on_Boton_jugar_mouse_entered() -> void:
+	$Boton_jugar.grab_focus()
+
+
+func _on_Boton_Creditos_mouse_entered() -> void:
+	$Boton_Creditos.grab_focus()
+
+
+func _on_Boton_salir_mouse_entered() -> void:
+	$Boton_salir.grab_focus()
